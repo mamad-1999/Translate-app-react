fee610d791ef6e897a200fb813b89f37b50a26bb
pc <www.mmhmdmm83@gmail.com>
2022-05-21 22:04:53 +0430

    Master: Merged header component in master

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+    return (
+        <header className='header'>
+            <div className="logo">
+                <span>ترجمه</span>
+            </div>
+        </header>
+    )
+}
+
+export default Header
