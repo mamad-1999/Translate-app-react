6607f496c36a34a861f0249d0bf792a3a32fe5f2
pc <www.mmhmdmm83@gmail.com>
2022-05-22 09:23:21 +0430

    textarea: fix textarea component and add icon Copy and Sound in component

--- src/components/TextArea/TextArea.jsx
+++ src/components/TextArea/TextArea.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import './TextArea.css'
+import { MdOutlineContentCopy } from 'react-icons/md'
+import { AiOutlineSound } from 'react-icons/ai'
 
 function TextArea({ holdertext }) {
     return (
         <div className='text_box'>
             <textarea placeholder={holdertext}></textarea>
+            <div className="text_icon_box">
+                <span className='icon'>
+                    <MdOutlineContentCopy />
+                </span>
+                <span className='icon'>
+                    <AiOutlineSound />
+                </span>
+            </div>
         </div>
     )
 }
